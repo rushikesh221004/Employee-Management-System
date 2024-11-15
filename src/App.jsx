edb1265d5f5bc73417edaf36ec3fb147fa700276
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2024-11-15 16:57:44 +0530

    Employee dashboard created

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Login from "./components/Auth/Login";
+import EmployeeDashboard from "./components/Dashboard/EmployeeDashboard";
 
 export default function App() {
   return (
     <>
-      <Login/>
+      {/* <Login/> */}
+      <EmployeeDashboard/>
     </>
   )
 }
---
+++ src/components/TaskList/TaskList.jsx
@@ -0,0 +1,41 @@
+export default function TaskList() {
+  return (
+    <div
+      id="tasklist"
+      className="h-[55%] flex overflow-x-auto px-3 items-center flex-nowrap gap-5 justify-start mt-10 py-5 w-full"
+    >
+      <div className="h-full flex-shrink-0 rounded-xl p-5 w-[450px] bg-red-400">
+        <div className="flex justify-between items-center ">
+            <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+            <h4 className="text-sm">25 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">Hello guys my name is rushikesh manjre and i am a fullstack engineer at google and i have a 10 years of experience</p>
+      </div>
+      <div className="h-full flex-shrink-0 rounded-xl p-5 w-[450px] bg-green-400">
+        <div className="flex justify-between items-center ">
+            <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+            <h4 className="text-sm">25 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">Hello guys my name is rushikesh manjre and i am a fullstack engineer at google and i have a 10 years of experience</p>
+      </div>
+      <div className="h-full flex-shrink-0 rounded-xl p-5 w-[450px] bg-blue-400">
+        <div className="flex justify-between items-center ">
+            <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+            <h4 className="text-sm">25 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">Hello guys my name is rushikesh manjre and i am a fullstack engineer at google and i have a 10 years of experience</p>
+      </div>
+      <div className="h-full flex-shrink-0 rounded-xl p-5 w-[450px] bg-yellow-400">
+        <div className="flex justify-between items-center ">
+            <h3 className="bg-red-600 text-sm px-3 py-1 rounded">High</h3>
+            <h4 className="text-sm">25 Feb 2024</h4>
+        </div>
+        <h2 className="mt-5 text-2xl font-semibold">Make a youtube video</h2>
+        <p className="text-sm mt-2">Hello guys my name is rushikesh manjre and i am a fullstack engineer at google and i have a 10 years of experience</p>
+      </div>
+    </div>
+  );
+}
